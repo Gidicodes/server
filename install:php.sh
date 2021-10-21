d0b37e5c563b764b98e4b11522cabd9de486030c
Idowu Emmanuel <idowu.immanuel17@gmail.com>
2021-10-21 19:25:28 +0100

    Added new ReadMe

--- install:php.sh
+++ install:php.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-sudo apt update
-sudo apt install php php-fpm php-redis php-imagick php-curl php-bcmath php-xmlrpc php-gd php-mysql php-dom php-cli php-json php-common php-mbstring php-opcache php-readline php-zip php-soap php-mongodb php-xml php-pear -y
 sudo apt install software-properties-common -y
 sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update
-sudo apt install php7.4 php7.4-gd php7.4-common -y
-
+sudo apt install php8.0 php8.0-common -y
+sudo apt install php8.0-fpm php8.0-mysql php8.0-redis php8.0-imagick php8.0-curl php8.0-bcmath php8.0-xmlrpc php8.0-gd php8.0-mysql php8.0-dom php8.0-cli php8.0-json php8.0-common php8.0-mbstring php8.0-opcache php8.0-readline php8.0-zip php8.0-soap php8.0-mongodb php8.0-xml php8.0-pear -y
 echo "To verify just run following command"
 echo "php -m"
 echo "php --ini
-
-sudo apt install php8.0-fpm
